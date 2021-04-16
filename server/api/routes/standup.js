4eac5e50f24f38199d744b5dacf7c90b7f3c47b4
HadIhab <chocovani101@hotmail.fr>
2021-04-16 18:29:42 +0100

    [Demo]: Querying Documents

--- server/api/routes/standup.js
+++ server/api/routes/standup.js
@@ -1,4 +1,5 @@
 const Standup = require('../../models/standup');
+const mongoose = require('mongoose');
 
 module.exports = function (router) {
 
@@ -14,6 +15,22 @@ module.exports = function (router) {
 		  			error: err
 		  		}));
 	});
+
+	router.get('/standup/:teamMemberId',(req,res) => {
+		const query = {
+			_teamMemberId: mongoose.Types.ObjectId(req.params.teamMemberId)
+		};
+		Standup.find(query)
+		  .sort({ 'createdOn': 1 })
+		  .exec()
+		  .then(docs => res.status(200)
+		  		.json(docs))
+		  .catch(err => res.status(500)
+		  		.json({
+		  			message: 'Error finding standup meeting notes for team Id',
+		  			error: err
+		  		}));
+	});	
 
 	router.post('/standup', function (req,res) {
 		let note = new Standup(req.body);
