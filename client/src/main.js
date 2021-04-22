63fa391b8a3995882254902eedae1a2f22d44d54
HadIhab <chocovani101@hotmail.fr>
2021-04-22 18:17:05 +0100

    Finalizing the Demo App for production

--- client/src/main.js
+++ client/src/main.js
@@ -1,14 +1,27 @@
 import Vue from 'vue'
-import App from './App.vue'
+import Vuetify from 'vuetify'
+import App from './App'
 import router from './router'
+import axios from 'axios'
+
+// Vuex store
 import store from './store'
-import vuetify from './plugins/vuetify';
+
+// Vuetify styles and icons
+import 'vuetify/dist/vuetify.min.css' // Ensure you are using css-loader
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
 Vue.config.productionTip = false
+
+Vue.use(Vuetify)
+
+// Setup axios to be available globally through Vue
+Vue.axios = Vue.prototype.$http = axios.create({
+  baseURL: 'http://localhost:8081/api'
+})
 
 new Vue({
   router,
   store,
-  vuetify,
   render: h => h(App)
 }).$mount('#app')
